8b69251bae8bdbb3cc390d29b640a8f81e071ab1
Trần Thế Vỹ <tranthevy@Hi-Admin.local>
2024-10-09 23:45:20 +0700

    update method post

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const morgan = require('morgan')
 const handlebars =require("express-handlebars") // import thu vien handerbars trong express js
 const path = require('path')
 
-//http loger hiem thi cac xu ly htpp trong terminal and console log
+//xử lý dữ liệu từ form data
+app.use(express.urlencoded({
+  extended:true
+}))
+//xử lý dữ liệu từ json (đoạn mã js gởi lên)
+app.use(express.json())
 
+//http loger hiem thi cac xu ly htpp trong terminal and console log
+// app.use(morgan('combined'))
 //dang ky cai name handlebars = funtion nay
 app.engine('hbs',handlebars.engine({extname : '.hbs', partialsDir: path.join(__dirname, 'resources','views', 'partials')}))
 //set view engie = cai name handlebars vua dang ky luc nay
@@ -21,15 +28,19 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 app.get('/news', (req, res) => {
-  console.log(req.query.Search);
   res.render('news')
 })
 app.get('/search', (req, res) => {
   console.log(req.query.Search);
-  
   res.render('search')
 })
-
+app.post('/search', function (req, res) {
+  console.log(req.body);
+  
+  res.send('')
+  
+  
+})
 app.listen(port, () => {
   console.log(`Click at here http://localhost:${port}`)
 })
